348aaf164705ef51f6d2bb2dfe1350b2ce9cbd50
Justine Lai <justine.lai@gmail.com>
2020-04-10 16:06:49 -0400

    set up basic auth0 SPA js SDK

---
+++ smock/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <div className="App">
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
