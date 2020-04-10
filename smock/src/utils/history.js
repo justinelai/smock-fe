6e23ac8fa55edb6c24246609c439fe51940be79c
Justine Lai <justine.lai@gmail.com>
2020-04-10 16:12:58 -0400

    work from development'

--- smock/src/utils/history.js
+++
@@ -1,5 +0,0 @@
-import { createBrowserHistory } from "history";
-export default createBrowserHistory();
-
-// https://auth0.com/docs/quickstart/spa/react
-//This file will be responsible for exporting react-router's history module so we can redirect the user programatically.
