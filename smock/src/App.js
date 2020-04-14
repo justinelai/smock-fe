cee7f6e698db7c8ad3e174790ef0efa80c10b75b
Justine Lai <justine.lai@gmail.com>
2020-04-14 18:15:05 -0400

    setup auth0 Expernal API ping test

--- smock/src/App.js
+++ smock/src/App.js
@@ -4,6 +4,7 @@ import { Router, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
 import history from "./utils/history";
 import PrivateRoute from "./components/PrivateRoute";
+import ExternalApi from "./components/ExternalApi"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Switch>
           <Route path="/" exact />
           <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </Router>
     </div>
